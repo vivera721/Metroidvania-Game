4c2d889dee097a6f58bbf1aea6b187aff46e0875
vivera721 <59154923+vivera721@users.noreply.github.com>
2022-03-30 02:31:20 +0900

    Abilities
    Double Jump, Dash, Dashing After-Image, Dash Limiting, Player Transform To Ball, Dropping Bombs, Fixing Passing Through Bug, Blowing Up Destructible Objects

---
+++ Metroidvania Game/Assets/Scripts/BombController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    public float timeToExplode = .5f;
+    public GameObject explosion;
+
+    public float blastRange;
+    public LayerMask whatIsDestructible;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        timeToExplode -= Time.deltaTime;
+        if (timeToExplode <= 0)
+        {
+            if (explosion != null)
+            {
+                Instantiate(explosion, transform.position, transform.rotation);
+            }
+
+            Destroy(gameObject);
+
+            Collider2D[] objectsToRemove = Physics2D.OverlapCircleAll(transform.position, blastRange, whatIsDestructible);
+
+            if (objectsToRemove.Length > 0)
+            {
+                foreach (Collider2D col in objectsToRemove)
+                {
+                    Destroy(col.gameObject);
+                }
+            }
+
+        }
+    }
+}
--- Metroidvania Game/Assets/Scripts/PlayerController.cs
+++ Metroidvania Game/Assets/Scripts/PlayerController.cs
@@ -18,6 +18,28 @@ public class PlayerController : MonoBehaviour
     public BulletController shotToFire;
     public Transform shotPoint;
 
+    private bool canDoubleJump;
+
+    public float dashSpeed, dashTime;
+    private float dashCounter;
+
+    public SpriteRenderer theSR, afterImage;
+    public float afterImageLifeTime, timeBetweenAfterImage;
+    private float afterImageCounter;
+    public Color afterImageColor;
+
+    public float waitAfterDashing;
+    private float dashRechargeCounter;
+
+    public GameObject standing, ball;
+    public float waitToBall;
+    private float ballCounter;
+
+    public Animator ballAnim;
+
+    public Transform bombPoint;
+    public GameObject bomb;
+
     void Start()
     {
         
@@ -26,38 +48,145 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        //move sideways
-        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
-
-        //handle direction change
-        if (theRB.velocity.x < 0)
+        if (dashRechargeCounter > 0)
         {
-            transform.localScale = new Vector3(-1f,1f,1f);
-        }
-        else if (theRB.velocity.x > 0)
-        {
-            transform.localScale = Vector3.one;
+            dashRechargeCounter -= Time.deltaTime;
         }
 
+        else
+        {
+            if (Input.GetButtonDown("Fire2") && standing.activeSelf)
+            {
+                dashCounter = dashTime;
+
+                ShowAfterImage();
+            } 
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            theRB.velocity = new Vector2(dashSpeed * transform.localScale.x, theRB.velocity.y);
+            //theRB.velocity = new Vector2(dashSpeed * transform.localScale.x,0f);
+
+            afterImageCounter -= Time.deltaTime;
+            if (afterImageCounter <= 0)
+            {
+                ShowAfterImage();
+            }
+
+            dashRechargeCounter = waitAfterDashing;
+
+        }
+        else
+        {
+
+            //move sideways
+            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
+
+            //handle direction change
+            if (theRB.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (theRB.velocity.x > 0)
+            {
+                transform.localScale = Vector3.one;
+            }
+
+        }
         //checking if on the ground
         isOnGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
 
         //jumping
-        if (Input.GetButtonDown("Jump") && isOnGround)
+        if (Input.GetButtonDown("Jump") && (isOnGround || canDoubleJump))
         {
+            if (isOnGround)
+            {
+                canDoubleJump = true;
+            }
+            else
+            {
+                canDoubleJump = false;
+
+                anim.SetTrigger("doubleJump");
+            }
+
             theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
         }
 
+        //shooting
         if (Input.GetButtonDown("Fire1"))
         {
-            Instantiate(shotToFire, shotPoint.position, shotPoint.rotation).moveDir = new Vector2(transform.localScale.x,0f);
+            if (standing.activeSelf)
+            {
+                Instantiate(shotToFire, shotPoint.position, shotPoint.rotation).moveDir = new Vector2(transform.localScale.x, 0f);
 
-            anim.SetTrigger("shotFired");
+                anim.SetTrigger("shotFired");
+            }
+            else if(ball.activeSelf)
+            {
+                Instantiate(bomb, bombPoint.position, bombPoint.rotation);
+            }
+        }
+
+        //ball mode
+        if (!ball.activeSelf)
+        {
+            if (Input.GetAxisRaw("Vertical") < -.9f)
+            {
+                ballCounter -= Time.deltaTime;
+                if (ballCounter <= 0)
+                {
+                    ball.SetActive(true);
+                    standing.SetActive(false);
+                }
+            }
+            else
+            {
+                ballCounter = waitToBall;
+            }
+        }
+        else
+        {
+            if (Input.GetAxisRaw("Vertical") > .9f)
+            {
+                ballCounter -= Time.deltaTime;
+                if (ballCounter <= 0)
+                {
+                    ball.SetActive(false);
+                    standing.SetActive(true);
+                }
+            }
+            else
+            {
+                ballCounter = waitToBall;
+            }
         }
 
 
 
-        anim.SetBool("isOnGround", isOnGround);
-        anim.SetFloat("speed",Mathf.Abs(theRB.velocity.x));
+        if (standing.activeSelf)
+        {
+            anim.SetBool("isOnGround", isOnGround);
+            anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
+        }
+        if (ball.activeSelf)
+        {
+            ballAnim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
+        }
     }
+
+    public void ShowAfterImage()
+    {
+        SpriteRenderer image = Instantiate(afterImage, transform.position, transform.rotation);
+        image.sprite = theSR.sprite;
+        image.transform.localScale = transform.localScale;
+        image.color = afterImageColor;
+
+        Destroy(image.gameObject, afterImageLifeTime);
+    }
+
+
 }
